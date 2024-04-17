0ea924aaafbcde70736a25ddbe0754389ae0dd0c
SharkFace34 <SharkFace34@mail.ru>
2024-04-17 18:37:08 +0300

    join division

--- main.py
+++ main.py
@@ -11,9 +11,15 @@ def additions(num1, num2):
 def subtraction(num1, num2):
     return print(num1 - num2)
 
+def division(num1, num2):
+    return print(num1 / num2)
+
 
 if operator == "+":
     additions(num1, num2)
 
 elif operator == "-":
     subtraction(num1, num2)
+
+elif operator == "/":
+    division(num1, num2)
